81bf64fccceedc86ebde66ba397b267fe647e8cd
Hunter Belanger <hunter.belanger@gmail.com>
2024-01-16 20:24:19 -0500

    Updates to link with BLAS and LAPACK for xtensor-blas linalg support.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Qanard
+
+## Dependencies
+This project requires BLAS and LAPACK to be installed on your system. If you
+are on a Debian-like system, you can install these with:
+
+```
+$ sudo apt-get install libblas-dev liblapack-dev
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <xtensor/xarray.hpp>
 #include <xtensor/xio.hpp>
-#include <xtensor/xview.hpp>
+#include <xtensor-blas/xlinalg.hpp>
 
 int main() {
-  xt::xarray<double> arr1
-    {{1.0, 2.0, 3.0},
-    {2.0, 5.0, 7.0},
-    {2.0, 5.0, 7.0}};
+  xt::xarray<double> A
+    {{1., 0., 1.},
+     {2., 1., 1.},
+     {0., 1., 1.},
+     {1., 1., 2.}};
 
-  xt::xarray<double> arr2 {5.0, 6.0, 7.0};
+  xt::xarray<double> B
+    {{1., 2., 1.},
+     {2., 3., 1.},
+     {4., 2., 2.}};
 
-  xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+  auto AB = xt::linalg::dot(A, B);
 
-  std::cout << res << "\n";
+  std::cout << AB << "\n";
 
   return 0;
 }
